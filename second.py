fb8bf4768880a23b698cb0ca7ad0d008106cb4db
Katie Greer <katieggreer@gmail.com>
2023-12-18 09:22:11 -0700

    added eat function to start.py, created second.py to use some start.py functions

---
+++ second.py
@@ -0,0 +1,12 @@
+#created this file
+
+
+from start import *
+
+
+
+def main():
+    eat(10)
+
+if __name__ == "__main__":
+    main()
--- start.py
+++ start.py
@@ -1,5 +1,13 @@
 
 
 def do_stuff(a):
-  print(a)
+  
+  print("we should make this function do more stuff")
+
+
+def eat(n):
+
+  for i in range(n):
+    print("crunch")
+    print(f"{i} poptarts eaten")
 
